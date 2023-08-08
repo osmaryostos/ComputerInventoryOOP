f67eb9b00c4bb1a3acb732d39c00c9877a9c019b
Osmary Ostos <osmary_ostos@hotmail.com>
2023-08-08 12:01:38 -0400

    Class Computer

---
+++ src/Demo.java
@@ -0,0 +1,9 @@
+public class Demo {
+    public static void main(String[] args) {
+        Computer c1 = new Computer();
+        Computer c2 = new Computer();
+
+Computer.findNumberOfCreatedComputers();
+    }
+}
+
